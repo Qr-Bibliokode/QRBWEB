94348d8f3a086f2741542ea0b3b99f009c25c414
felansu <gaferran@gmail.com>
2015-09-05 19:37:36 -0300

    #1 - Temporaly solution for persist date

--- src/client/main/app/components/holiday/create/holiday.create.controller.js
+++ src/client/main/app/components/holiday/create/holiday.create.controller.js
@@ -3,13 +3,15 @@
 
     angular
         .module('qrbweb')
-        .controller('HolidayCreateController', ['HolidayFactory', 'MessageFactory', HolidayCreateController]);
+        .controller('HolidayCreateController', ['HolidayFactory', 'MessageFactory', '$filter', HolidayCreateController]);
 
     /** @ngInject */
-    function HolidayCreateController(HolidayFactory, MessageFactory) {
+    function HolidayCreateController(HolidayFactory, MessageFactory, $filter) {
         var vm = this;
 
         vm.create = function () {
+            vm.holiday.startDate = $filter('date')(vm.holiday.startDate, "yyyy-MM-dd HH:mm:ss.s");
+            vm.holiday.finalDate = $filter('date')(vm.holiday.finalDate, "yyyy-MM-dd HH:mm:ss.s");
             HolidayFactory.create(vm.holiday).then(function () {
                 MessageFactory.success('Feriado ' + vm.holiday.description + ' salvo com sucesso.');
                 vm.clear();
