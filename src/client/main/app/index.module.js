185f64d366cf86cfc8448f6f693c9b41a3efbe1b
felansu <gaferran@gmail.com>
2015-11-11 02:49:37 -0200

    #31 - Acrescentado locale provider, com isto já é possível preencher com a data no padrão brasileiro.

--- src/client/main/app/index.module.js
+++ src/client/main/app/index.module.js
@@ -1,1 +1,20 @@
-angular.module('qrbweb', ['ui.router','md.data.table', 'ngMaterial', 'monospaced.qrcode']);
+angular
+
+    .module('qrbweb', ['ui.router', 'md.data.table', 'ngMaterial', 'monospaced.qrcode'])
+
+    .config(function ($mdDateLocaleProvider) {
+        $mdDateLocaleProvider.months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+        $mdDateLocaleProvider.shortMonths = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+        $mdDateLocaleProvider.days = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo'];
+        $mdDateLocaleProvider.shortDays = ['seg', 'ter', 'qua', 'qui', 'sex', 'sab', 'dom'];
+
+        $mdDateLocaleProvider.firstDayOfWeek = 1;
+
+        $mdDateLocaleProvider.parseDate = function (dateString) {
+            return new Date(dateString);
+        };
+
+        $mdDateLocaleProvider.formatDate = function (date) {
+            return moment(date).format('DD-MM-YYYY');
+        };
+    });
